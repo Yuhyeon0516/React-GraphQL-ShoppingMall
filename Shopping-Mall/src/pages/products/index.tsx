a04b758de477226d8aac35d8af748c334a34524e
Yuhyeon0516 <rladbgus0516@naver.com>
2023-10-11 21:51:54 +0900

    Mock data was imported using useQuery.

--- Shopping-Mall/src/pages/products/index.tsx
+++ Shopping-Mall/src/pages/products/index.tsx
@@ -1,5 +1,28 @@
-import React from 'react';
+import { useQuery } from 'react-query';
+import { QueryKeys, fetcher } from '../../utils/queryClient';
+import { ProductItemType } from '../../types/types';
 
 export default function ProductList() {
+    const { data } = useQuery<ProductItemType[]>(QueryKeys.PRODUCTS, () => fetcher({ method: 'GET', path: '/products' }));
+
+    /*
+    id: 58
+    title: "Practical Granite Car"
+    price: 240
+    description: "Andy shoes are designed to keeping in mind durability as well as trends, the most stylish range of shoes & sandals"
+    ▶ images 3 Array
+        0: "https://i.imgur.com/DumuKkD.jpeg"
+        1: "https://i.imgur.com/OARGZQW.jpeg"
+        2: "https://i.imgur.com/x0K3SKA.jpeg"
+    creationAt: "2023-10-11T03:24:10.000Z"
+    updatedAt: "2023-10-11T11:42:22.000Z"
+    ▶ category 5 Object
+        id: 4
+        name: "Shoes"
+        image: "https://i.imgur.com/x0K3SKA.jpeg"
+        creationAt: "2023-10-11T03:24:10.000Z"
+        updatedAt: "2023-10-11T03:24:10.000Z"
+    */
+
     return <div>ProductList</div>;
 }
--- Shopping-Mall/src/types/types.ts
+++ Shopping-Mall/src/types/types.ts
@@ -1,10 +1,29 @@
-export type AnyOBJ = {
+export interface AnyOBJ {
     [key: string]: any;
-};
+}
 
 export interface FetcherPropsType {
     method: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH';
     path: string;
     body?: AnyOBJ;
     params?: AnyOBJ;
+}
+
+interface ProductCategory {
+    id: number;
+    name: string;
+    image: string;
+    creationAt: Date;
+    updatedAt: Date;
+}
+
+export interface ProductItemType {
+    id: number;
+    title: string;
+    price: number;
+    description: string;
+    images: string[];
+    creationAt: Date;
+    updatedAt: Date;
+    category: ProductCategory;
 }
