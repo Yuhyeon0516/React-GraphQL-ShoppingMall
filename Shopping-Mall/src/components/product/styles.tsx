10986554f0750c96b1bf7e7553bf27186a11f359
Yuhyeon0516 <rladbgus0516@naver.com>
2023-10-12 12:45:02 +0900

    Created floating button.

--- Shopping-Mall/src/components/product/styles.tsx
+++ Shopping-Mall/src/components/product/styles.tsx
@@ -2,15 +2,15 @@ import styled from 'styled-components';
 
 export const ProductListContainer = styled.div`
     display: grid;
-    grid-template-columns: repeat(3, 1fr);
+    grid-template-columns: repeat(3, auto);
     gap: 15px;
 
     @media screen and (max-width: 1400px) {
-        grid-template-columns: repeat(2, 1fr);
+        grid-template-columns: repeat(2, auto);
     }
 
     @media screen and (max-width: 900px) {
-        grid-template-columns: repeat(1, 1fr);
+        grid-template-columns: repeat(1, auto);
     }
 `;
 
@@ -18,13 +18,14 @@ export const ProductItemContainer = styled.div`
     border: 3px solid #65ceb0;
     border-radius: 20px;
     display: flex;
+    flex-direction: column;
     flex: 1;
-    flex-direction: row;
     padding: 10px;
 `;
 
 export const ProductItemImage = styled.img`
-    height: 200px;
+    width: 90%;
+    align-self: center;
     border-radius: 20px;
     overflow: hidden;
 `;
@@ -34,7 +35,8 @@ export const ProductItemTextContainer = styled.div`
     flex: 1;
     flex-direction: column;
     gap: 5px;
-    margin-left: 10px;
+    margin-top: 20px;
+    margin-left: 20px;
 `;
 
 export const ProductItemColumn = styled.div`
@@ -54,5 +56,20 @@ export const ProductItemTextType = styled.h4`
 export const ProdcutItemText = styled.p`
     margin: 0;
     font-size: 16px;
+`;
+
+export const FloatingButton = styled.button`
+    background-color: #65ceb0;
+    border-radius: 100px;
+    border-width: 0px;
+    display: flex;
     flex: 1;
+    align-items: center;
+    justify-content: center;
+    position: fixed;
+    width: 60px;
+    height: 60px;
+    right: 20px;
+    bottom: 20px;
+    cursor: pointer;
 `;
