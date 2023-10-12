0da8170f808bc684b7c7391c7844424a2dceed26
Yuhyeon0516 <rladbgus0516@naver.com>
2023-10-12 18:05:57 +0900

    Created cart state management using recoil.

--- Shopping-Mall/src/components/product/item.tsx
+++ Shopping-Mall/src/components/product/item.tsx
@@ -12,32 +12,38 @@ import {
 import { Product } from '../../types/types';
 
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { useRecoilState } from 'recoil';
+import { cartSelector } from '../../recoil/cart';
+import { useCallback } from 'react';
 
 export default function ProductItem({ id, title, price, imageUrl, createdAt }: Product) {
+    const [cartAmmount, setCartAmmount] = useRecoilState(cartSelector(id));
+
+    const addToCart = useCallback(() => setCartAmmount((cartAmmount || 0) + 1), [cartAmmount, setCartAmmount]);
+
     return (
-        <div>
-            <ProductItemContainer>
-                <StyledLink to={`/products/${id}`}>
-                    <ProductItemImage src={imageUrl} alt="image" />
-                    <ProductItemTextContainer>
-                        <ProductItemColumn>
-                            <ProductItemTextType>Product Name :</ProductItemTextType>&emsp;
-                            <ProdcutItemText>{title}</ProdcutItemText>
-                        </ProductItemColumn>
+        <ProductItemContainer>
+            <StyledLink to={`/products/${id}`}>
+                <ProductItemImage src={imageUrl} alt="image" />
+                <ProductItemTextContainer>
+                    <ProductItemColumn>
+                        <ProductItemTextType>Product Name :</ProductItemTextType>&emsp;
+                        <ProdcutItemText>{title}</ProdcutItemText>
+                    </ProductItemColumn>
 
-                        <ProductItemColumn>
-                            <ProductItemTextType>Price :</ProductItemTextType>&emsp;
-                            <ProdcutItemText>$ {price}</ProdcutItemText>
-                        </ProductItemColumn>
+                    <ProductItemColumn>
+                        <ProductItemTextType>Price :</ProductItemTextType>&emsp;
+                        <ProdcutItemText>$ {price}</ProdcutItemText>
+                    </ProductItemColumn>
 
-                        <ProductItemColumn>
-                            <ProductItemTextType>Create :</ProductItemTextType>&emsp;
-                            <ProdcutItemText>{moment(createdAt).format('YYYY/MM/DD hh:mm:ss').toString()}</ProdcutItemText>
-                        </ProductItemColumn>
-                    </ProductItemTextContainer>
-                </StyledLink>
-                <CartButtonIcon icon={faCartShopping} onClick={() => console.log('click')} />
-            </ProductItemContainer>
-        </div>
+                    <ProductItemColumn>
+                        <ProductItemTextType>Create :</ProductItemTextType>&emsp;
+                        <ProdcutItemText>{moment(createdAt).format('YYYY/MM/DD hh:mm:ss').toString()}</ProdcutItemText>
+                    </ProductItemColumn>
+                </ProductItemTextContainer>
+            </StyledLink>
+            <CartButtonIcon icon={faCartShopping} onClick={addToCart} />
+            <span>{cartAmmount || 0}</span>
+        </ProductItemContainer>
     );
 }
