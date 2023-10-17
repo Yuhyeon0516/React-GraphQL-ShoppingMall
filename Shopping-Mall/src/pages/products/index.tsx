57a9092a95e3c8a0086349290fbc2c0e60f1b8f0
Yuhyeon0516 <rladbgus0516@naver.com>
2023-10-17 18:44:37 +0900

    Applied infinite scroll to FE.

--- Shopping-Mall/src/pages/products/index.tsx
+++ Shopping-Mall/src/pages/products/index.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from 'react-query';
+import { useInfiniteQuery } from 'react-query';
 import { QueryKeys, graphqlFetcher } from '../../utils/queryClient';
 import ProductItem from '../../components/product/item';
 import { FloatingButton, ProductListContainer } from '../../styles/styles';
@@ -6,19 +6,47 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { GET_PRODUCTS } from '../../graphql/products';
 import { Products } from '../../types/types';
+import { useEffect, useRef } from 'react';
+import useInfiniteScroll from '../../hooks/useInfiniteScroll';
 
 export default function ProductList() {
-    // const { data } = useQuery<ProductItemType[]>(QueryKeys.PRODUCTS, () => fetcher({ method: 'GET', path: '/products' }));
-    const { data } = useQuery<Products>(QueryKeys.PRODUCTS, () => graphqlFetcher<Products>(GET_PRODUCTS));
+    const { data, fetchNextPage, hasNextPage, isFetchingNextPage } = useInfiniteQuery<Products>(
+        QueryKeys.PRODUCTS,
+        ({ pageParam = '' }) => graphqlFetcher<Products>(GET_PRODUCTS, { cursor: pageParam }),
+        {
+            getNextPageParam: (lastPage) => {
+                return lastPage.products.at(-1)?.id;
+            },
+        },
+    );
+    const fetchMoreRef = useRef<HTMLDivElement | null>(null);
+    const isFirstFetch = useRef(true);
+    const intersecting = useInfiniteScroll(fetchMoreRef);
+
+    useEffect(() => {
+        if (!intersecting) return;
+        if (isFirstFetch.current) {
+            isFirstFetch.current = false;
+            return;
+        }
+        if (hasNextPage && !isFetchingNextPage) {
+            fetchNextPage();
+        }
+    }, [intersecting]);
 
     return (
-        <ProductListContainer>
-            {data?.products.map((product) => (
-                <ProductItem {...product} key={product.id} />
-            ))}
-            <FloatingButton>
-                <FontAwesomeIcon icon={faPlus} fontSize={18} color="white" />
-            </FloatingButton>
-        </ProductListContainer>
+        <>
+            <ProductListContainer>
+                {data?.pages.map((page) =>
+                    page.products.map((product) => {
+                        return <ProductItem {...product} key={product.id} />;
+                    }),
+                )}
+                <FloatingButton>
+                    <FontAwesomeIcon icon={faPlus} fontSize={18} color="white" />
+                </FloatingButton>
+            </ProductListContainer>
+            <div ref={fetchMoreRef} />
+        </>
     );
 }
